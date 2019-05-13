46a185eff8285b63f445e2b1bd0a28c6df1eb7f5
Kayla Meiklejohn <kaysway90s@gmail.com>
2019-05-12 23:07:23 -0600

    schema

--- model.js
+++ model.js
@@ -2,3 +2,13 @@
 
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
+
+const blogSchema = mongoose.Schema({
+    author: {
+      firstName: String,
+      lastName: String
+    },
+    title: {type: String, required: true},
+    content: {type: String},
+    created: {type: Date, default: Date.now}
+  });
