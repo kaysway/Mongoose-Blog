2c4573caba3880c78cec101234612970a1fabcdf
Kayla Meiklejohn <kaysway90s@gmail.com>
2019-05-12 23:06:00 -0600

    adding model.js schema function

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/posts', (req, res) => {
       })
       .catch(err => {
         console.error(err);
-        res.status(500).json({ error: 'something went terribly wrong' });
+        res.status(500).json({ error: 'something went wrong' });
       });
   });
 
@@ -29,14 +29,48 @@ app.get('/posts', (req, res) => {
       .then(post => res.json(post.serialize()))
       .catch(err => {
         console.error(err);
-        res.status(500).json({ error: 'something went horribly awry' });
+        res.status(500).json({ error: 'something went wrong' });
       });
   });
 
+  app.post('/posts', (req, res) => {
+    const requiredFields = ['title', 'content', 'author'];
+    for (let i = 0; i < requiredFields.length; i++) {
+      const field = requiredFields[i];
+      if (!(field in req.body)) {
+        const message = `Missing \`${field}\` in request body`;
+        console.error(message);
+        return res.status(400).send(message);
+      }
+    }
+  
+    BlogPost
+      .create({
+        title: req.body.title,
+        content: req.body.content,
+        author: req.body.author
+      })
+      .then(blogPost => res.status(201).json(blogPost.serialize()))
+      .catch(err => {
+        console.error(err);
+        res.status(500).json({ error: 'Something went wrong' });
+      });
+  
+  });
 
+  // app.delete and app.put next....
 
+  //app.delete(){
 
+ // }
 
+ // app.put(){
+
+ // }
+
+  app.use('*', function (req, res) {
+    res.status(404).json({ message: 'Not Found' });
+  });
 
 // closeServer needs access to a server object, but that only
 // gets created when `runServer` runs, so we declare `server` here
