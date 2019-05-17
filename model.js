4e51202da4decfbe220082ed9b95cefb775465c1
Kayla Meiklejohn <kaysway90s@gmail.com>
2019-05-16 18:43:51 -0600

    updated endpoints and blog schema

--- model.js
+++ model.js
@@ -3,7 +3,18 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-const blogSchema = mongoose.Schema({
+let authorSchema = mongoose.Schema({
+  firstName: 'string',
+  lastName: 'string',
+  userName: {
+      type: 'string',
+      unique: true
+  }
+});
+
+let commentSchema = mongoose.Schema({ content: 'string' });
+
+let blogSchema = mongoose.Schema({
     author: {
       firstName: String,
       lastName: String
@@ -12,3 +23,33 @@ const blogSchema = mongoose.Schema({
     content: {type: String},
     created: {type: Date, default: Date.now}
   });
+
+  blogPostSchema.pre('find', function(next){
+    this.populate('author');
+    next();
+  });
+
+  blogPostSchema.pre('findOne', function(next) {
+    this.populate('author');
+    next();
+  });
+
+  blogPostSchema.virtual('authorName').get(function() {
+    return `${this.author.firstName}`.trim();
+  });
+
+  blogPostSchema.methods.serialize = function() {
+    return {
+      id: this._id,
+      author: this.authorName,
+      content: this.content,
+      title: this.title,
+      comments: this.comments
+    };
+  };
+
+  let Author = mongoose.model('Author', authorSchema);
+  const BlogPost = mongoose.model('BlogPost', blogPostSchema);
+
+  module.exports = {Author, BlogPost};
+
